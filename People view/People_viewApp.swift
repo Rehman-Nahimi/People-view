94b405b3fb03b76f45a1bd3052defa3ae474f71f
Rehman Nahimi <rehman2000@live.com>
2023-10-22 15:42:17 +0100

    Initial Commit

---
+++ People view/People_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  People_viewApp.swift
+//  People view
+//
+//  Created by Ray Nahimi on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct People_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
